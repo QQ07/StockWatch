299816eac64848a9f62e20e761c94a515db10bf6
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2024-05-20 14:58:29 +0530

    image dimensions set

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-"use client"
 import { Dashboard } from "@/components/component/dashboard";
-import useWindowDimensions from "@/lib/windowDimensions";
 import Image from "next/image";
 import homeScreen from "@/app/homeScreen.jpg";
 export default function Home() {
-  const { height, width } = useWindowDimensions();
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold text-blue-400 mb-32">
@@ -14,8 +10,8 @@ export default function Home() {
       <Image
         src={homeScreen}
         alt="Stock growth image"
-        width={width/2}
-        height={height/2}
+        width={720}
+        height={400}
       />
       <Dashboard />
       <br />
